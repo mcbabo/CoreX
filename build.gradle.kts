ea55969e204ca8f0cdff334f5a18650dbfb2c840
mcbabo <moritz.joksch@aon.at>
2025-08-18 20:26:23 +0200

    updated kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
 
-    id("com.google.devtools.ksp") version "2.2.0-2.0.2" apply false
+    id("com.google.devtools.ksp") version "2.2.10-2.0.2" apply false
 
     id("com.google.dagger.hilt.android") version "2.57" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.0" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.10" apply false
 }
